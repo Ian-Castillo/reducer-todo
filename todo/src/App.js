e0b1491405ed7972d8b96cf259347c4f288047f7
Ian Castillo <iancastillo@Ians-MacBook-Pro.local>
2020-04-12 21:32:07 -0500

    MVP

---
+++ todo/src/App.js
@@ -0,0 +1,35 @@
+  
+import React, { useReducer } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Reducer, initialState } from "./Reducers/Reducer.js";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+
+  const [state, dispatch] = useReducer(Reducer, initialState)
+
+  const addItem = (item) => {
+    const newItem = {
+      id: Date.now(),
+      completed: false,
+      item: item
+    }
+    dispatch({ type: "ADD_ITEM", payload: newItem })
+  }
+
+  const toggleCompleted = (item) => {
+    console.log(item);
+    dispatch({ type: "TOGGLE_COMPLETED", payload: item })
+  }
+
+  return (
+    <div className="App">
+      <TodoForm addItem={addItem} clearCompleted={() => dispatch({ type: "CLEAR_COMPLETED" })} />
+      <TodoList todo={state} toggleCompleted={toggleCompleted} />
+    </div>
+  );
+}
+
+export default App;
